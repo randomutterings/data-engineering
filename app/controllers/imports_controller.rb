1b8050310e58a436023937f3f1e27c90066dcc8a
Chris Barnes <randomutterings@gmail.com>
2014-04-04 15:19:45 -0400

    Added routes for imports

---
+++ app/controllers/imports_controller.rb
@@ -0,0 +1,3 @@
+class ImportsController < ApplicationController
+
+end
---
+++ spec/routing/routes_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+describe "Routing" do
+  it { should route(:get, '/imports/new').to('imports#new') }
+  it { should route(:post, '/imports').to('imports#create') }
+  it { should route(:get, '/imports/1').to('imports#show', id: '1') }
+
+  it "does not route to #index" do
+    get("/imports").should_not be_routable
+  end
+
+  it "does not route to #edit" do
+    get("/imports/1/edit").should_not be_routable
+  end
+
+  it "does not route to #update" do
+    put("/imports/1").should_not be_routable
+  end
+
+  it "does not route to #destroy" do
+    delete("/imports/1").should_not be_routable
+  end
+
+end
