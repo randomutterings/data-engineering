66ec898c1128d81ffbc0d53dee1f1a8bbc8e1e5a
Chris Barnes <randomutterings@gmail.com>
2014-04-04 19:42:46 -0400

    Removed test twitter config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :twitter, "GZD8uIgd42Ys3PzLSYhsCyJIs", "vcQgMgYgwpyVZXd4b8P69oY1PLfXpcYl1f01uxPTpKTlx8D4AK"
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
